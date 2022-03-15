588e929ecb46ef5c38c38e68b1b4bfeb22bdd080
saocebri <albacarlos2012@gmail.com>
2022-03-14 22:45:27 -0300

    Agora Vai

--- projeto1-app/src/App.jsx
+++ projeto1-app/src/App.jsx
@@ -1,38 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React, { Component } from 'react'
+import Demo from './componentes/Demo'
 
 export default class App extends Component {
 
-  endereco() {
+  /* endereco() {
     const endereco = 'src/App.jsx'
     return endereco;
   }
 
-  soma(n1,n2){
-    var total = n1+n2;
+  soma(n1, n2) {
+    var total = n1 + n2;
     return total;
-  }
+  } */
   render() {
     return (
-      <div>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>{this.endereco()}</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
+
+      <div className="App">
+       <Demo />
       </div>
+
     )
   }
 }
--- projeto1-app/src/componentes/Demo.jsx
+++ projeto1-app/src/componentes/Demo.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
 export default class Demo extends Component {
-  render() {
-    return (
-      <div>Demo</div>
-      
-    )
-  }
+    nomes() {
+        const nomes = 'Carlos Eduardo';
+        return nomes;
+    }
+    render() {
+        return (
+            <div>
+                <h1>{this.nomes()}</h1>
+                <h2>Hello World!! Reactjs</h2>
+            </div>
+        )
+    }
 }
